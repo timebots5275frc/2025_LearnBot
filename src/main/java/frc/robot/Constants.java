2bad63cb513a031237ed7093acbb7f2c46b675b0
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2025-12-11 20:51:07 -0600

    quick fix

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,27 +16,12 @@ import frc.robot.CustomTypes.PID;
  */
 public final class Constants {
 
-  public static final double INTAKE_PIVOT_ROTATIONS_PER_DEGREE = (270.0/360.0)/*(270/(double)360)*/;
+  
   
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
 
   }
 
-  public final class ElevatorConstants {
-    public static final PID ELEVATOR_PID = new PID(0,0,0,0,0);//idk 
-
-
-    public static final int ELEVATOR_MOTOR_1_ID = 41;
-    public static final int ELEVATOR_MOTOR_2_ID = 42;
-
-    public static final double LEVEL_TWO = (INTAKE_PIVOT_ROTATIONS_PER_DEGREE * (360*.07));
-    public static final double LEVEL_THREE = (INTAKE_PIVOT_ROTATIONS_PER_DEGREE * (360*.14));
-    public static final double LEVEL_FOUR = (INTAKE_PIVOT_ROTATIONS_PER_DEGREE * (360*.27));
-    public static final double DRIVE      = 0;
-    public static final double INTAKE     = (INTAKE_PIVOT_ROTATIONS_PER_DEGREE * (360*.0233));
-    public static final double ALGAE =INTAKE_PIVOT_ROTATIONS_PER_DEGREE*360*.2;
-
-
-  }
+  
 }
