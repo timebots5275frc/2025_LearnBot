61fb062a7cda994128559cda353543d0c23d4692
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2025-12-12 21:57:08 -0600

    fix

--- src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -1,18 +1,8 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.signals.ControlModeValue;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+
 
 public class AlgaeIntakeSubsystem extends SubsystemBase{
 
