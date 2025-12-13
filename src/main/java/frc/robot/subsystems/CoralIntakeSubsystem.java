8db1257b712d0843c9764ae1566aacdcb985c4c1
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2025-12-12 21:57:46 -0600

    fix

--- src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
@@ -1,15 +1,9 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkMax;
+
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.CustomTypes.PID;
+
 
 public class CoralIntakeSubsystem extends SubsystemBase{
     private LaserCANSubsystem laserCANSubsystem;
