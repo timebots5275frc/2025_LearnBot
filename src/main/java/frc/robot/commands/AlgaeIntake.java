5ffc1d14daa8c648aa90bab750e22fe4462815c5
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2026-01-08 18:59:28 -0600

    si

---
+++ src/main/java/frc/robot/commands/AlgaeIntake.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class AlgaeIntake extends InstantCommand {
+
+    public AlgaeIntake() {
+
+    }
+
+}
